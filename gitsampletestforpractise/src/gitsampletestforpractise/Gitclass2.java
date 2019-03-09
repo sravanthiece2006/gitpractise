b8a2b9706cb923ccd9835fa850813adf022878d9
sravanthiece2006 <sravanthiece2006gmail.com>
2019-03-08 21:20:54 -0500

    commit4 updated gitclass2 and created git class3

--- gitsampletestforpractise/src/gitsampletestforpractise/Gitclass2.java
+++ gitsampletestforpractise/src/gitsampletestforpractise/Gitclass2.java
@@ -3,5 +3,6 @@ package gitsampletestforpractise;
 public class Gitclass2 {
 	
 	//this git class 2 project
+	//created new brach with name sai
 
 }
---
+++ gitsampletestforpractise/src/gitsampletestforpractise/gitclass3.java
@@ -0,0 +1,5 @@
+package gitsampletestforpractise;
+
+public class gitclass3 {
+
+}
